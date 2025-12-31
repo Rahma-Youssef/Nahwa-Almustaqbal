21cdd48418ed067331c7e0274f88478bd796119c
Rahma1219 <rahmayoussef920@gmail.com>
2025-12-31 17:39:06 +0200

    change address

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
                             <Icon icon="line-md:phone" width="24" height="24" className='text-blue-950 bg-[#F4A711] rounded-lg p-2 w-10 h-10' />
                             <div>
                                 <h4 className="md:text-xl font-semibold mb-2">Eng. Ahmed Mounir</h4>
-                                <p className="flex items-center gap-2 font-semibold font-mono text-blue-700">
+                                <p className="flex items-center gap-2 font-semibold  md:text-md   text-sm  font-mono text-blue-700">
                                     <a href="https://wa.me/971582598387" target='_blank' className="">+971582598387</a>
 
                                 </p>
@@ -34,7 +34,7 @@ const Contact = () => {
                             <Icon icon="line-md:phone" width="24" height="24" className='text-blue-950 bg-[#F4A711] rounded-lg p-2 w-10 h-10' />
                             <div>
                                 <h4 className="md:text-xl font-semibold mb-2">Eng. Ashraf Zain</h4>
-                                <p className="flex items-center gap-2 font-semibold font-mono text-blue-700">
+                                <p className="flex items-center gap-2 font-semibold font-mono md:text-md   text-sm  text-blue-700">
                                     <a href="https://wa.me/971589803373" target='_blank' className="">+971589803373</a>
 
                                 </p>
@@ -62,14 +62,12 @@ const Contact = () => {
                                 </div>
                             </div>
                             <div className="mt-2">
-                                <p className="  font-semibold font-mono text-left">
-                                    Amr Bin Al A'as St - Hay Mogader - Sharjah - United Arab Emirates
+                                <p className="  font-semibold font-mono text-right md:text-md text-sm">
+                                    كلباء-الخوير/كلباء- شارع ابوبكر الرازي مكتب ملك عائشة يوسف محمد
                                 </p>
                             </div>
                         </div>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.2282618820714!2d56.355033000000006!3d25.0602514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ef4fbd049e774ed%3A0xcb934695c0d19a35!2z2YbYrdmIINin2YTZhdiz2KrZgtio2YQg2YTZhNmF2YLYp9mI2YTYp9iqINin2YTYudin2YXYqQ!5e0!3m2!1sen!2seg!4v1767081635500!5m2!1sen!2seg" className={`border-0 rounded-lg w-full md:h-50 h-40 `} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-
 
 
 
